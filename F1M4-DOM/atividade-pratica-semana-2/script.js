2be5c5fac32e48f787b6f41a57f40088bb5e1af9
f2lpOliveira <contato@fellipefreire.com.br>
2023-02-01 13:08:54 -0300

    Resolução Atividade Prática da Semana 2

--- F1M4-DOM/atividade-pratica-semana-2/script.js
+++ F1M4-DOM/atividade-pratica-semana-2/script.js
@@ -1,24 +1,57 @@
-var cachorros = [{ "nome": "Thor", "imagem": "dog1" }, { "nome": "Zeca", "imagem": "dog2" }, { "nome": "Zeus", "imagem": "dog3" }, { "nome": "Pingo", "imagem": "dog4" }];
+var cachorros = [
+  { nome: "Thor", imagem: "dog1" },
+  { nome: "Zeca", imagem: "dog2" },
+  { nome: "Zeus", imagem: "dog3" },
+  { nome: "Pingo", imagem: "dog4" },
+];
 
-var gatos = [{ "nome": "Dunga", "imagem": "cat1" }, { "nome": "Mika", "imagem": "cat2" }, { "nome": "Conan", "imagem": "cat3" }, { "nome": "Messi", "imagem": "cat4" }];
+var gatos = [
+  { nome: "Dunga", imagem: "cat1" },
+  { nome: "Mika", imagem: "cat2" },
+  { nome: "Conan", imagem: "cat3" },
+  { nome: "Messi", imagem: "cat4" },
+];
 
 // Selecionar elemento 'select_animais' ( 1 ponto)
+let select_animais = document.getElementById("select_animais");
 // Selecionar elemento 'container' ( 1 ponto)
+let container = document.getElementById("container");
 // Selecionar elemento 'nome' ( 1 ponto)
-
+let nome = document.getElementById("nome");
 // Adicionar Listener 'input' ao elemento 'select_animais' (1 ponto)
+select_animais.addEventListener("input", listarAnimais);
 // Adicionar Listener 'mouseover' ao elemento 'container' (1 ponto)
+container.addEventListener("mouseover", mostrarNome);
 // Adicionar Listener 'mouseout' ao elemento 'container' (1 ponto)
+container.addEventListener("mouseout", limparNome);
 
-function listarAnimais() { // Implementação da função (10 pontos)
+function listarAnimais() {
+  // Implementação da função (10 pontos)
   limparDivContainer();
   // Implementar...
   // Criar elementos do tipo <img src=./img/....
   // Fazer com que os elementos img sejam filhos do elemento 'container'
+  if (select_animais.value == "dog") {
+    for (var i = 0; i < cachorros.length; i++) {
+      let img = document.createElement("img");
+      img.src = "./img/dog" + [i + 1] + ".jpg";
+      img.alt = cachorros[i].nome;
+      container.appendChild(img);
+    }
+  } else {
+    for (var i = 0; i <= cachorros.length; i++) {
+      let img = document.createElement("img");
+      img.src = "./img/cat" + [i + 1] + ".jpg";
+      img.alt = gatos[i].nome;
+      container.appendChild(img);
+    }
+  }
 }
 
-function mostrarNome(e) { // Implementação da função (4 pontos)
+function mostrarNome(e) {
+  // Implementação da função (4 pontos)
   // Implementar...
+  nome.innerText = e.target.alt;
 }
 
 function limparNome() {
